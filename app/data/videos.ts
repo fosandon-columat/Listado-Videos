1e77b8c89540f43e251bd24baef351d55cea757a
fosandon-columat <fosandon@columat.com>
2026-01-09 13:54:01 +0100

    Actualizar categorías y videos en la lista de recursos

--- app/data/videos.ts
+++ app/data/videos.ts
@@ -10,8 +10,9 @@ export const categories: string[] = [
   'General',
   'Onboarding',
   'Instalaciones',
-  'Seguridad',
-  'Recursos Humanos'
+  'Applocker',
+  'Recursos Humanos',
+  'Reservas'
 ];
 
 export const videos: Video[] = [
@@ -20,41 +21,21 @@ export const videos: Video[] = [
     title: 'Instalación de App Locker Logistic',
     description: 'Instalar app locker Logistic, en un ordenador planchado con error de usuario fantasma.',
     url: 'https://drive.google.com/file/d/1rlLaxxN35jU5yT0olF6slJfO0m3r2LGn/view?usp=sharing',
-    category: 'Instalaciones'
+    category: 'Applocker'
   },
   {
     id: 2,
-    title: 'Configuración del correo',
-    description: 'Paso a paso para configurar tu correo corporativo.',
-    url: 'https://drive.google.com/icon?id=2',
-    category: 'Herramientas'
+    title: 'Actualizar 3.1 Parcel',
+    description: 'Paso a paso para actualizar a la versión 3.1 de Parcel.',
+    url: 'https://drive.google.com/file/d/16bkuRHrTvV6JNwk9lgP24YLW3SsCK9aM/view?usp=sharing',
+    category: 'Applocker'
   },
   {
     id: 3,
-    title: 'Normas de Convivencia',
-    description: 'Reglas básicas para el uso de espacios comunes.',
-    url: 'https://drive.google.com/icon?id=3',
-    category: 'Onboarding'
+    title: 'Reservas',
+    description: 'Capacitación sobre la lógica de reservas.',
+    url: 'https://drive.google.com/file/d/1T8qouAJEjwmpLASFpIO2IOz_IZoJkIrN/view?usp=sharing',
+    category: 'Reservas'
   },
-  {
-    id: 4,
-    title: 'Protocolo de Seguridad',
-    description: 'Qué hacer en caso de emergencia.',
-    url: 'https://drive.google.com/icon?id=4',
-    category: 'Seguridad'
-  },
-   {
-    id: 5,
-    title: 'Beneficios y Compensaciones',
-    description: 'Explicación detallada de los beneficios laborales.',
-    url: 'https://drive.google.com/icon?id=5',
-    category: 'Recursos Humanos'
-  },
-   {
-    id: 6,
-    title: 'Uso de Slack',
-    description: 'Cómo comunicarse efectivamente usando Slack.',
-    url: 'https://drive.google.com/icon?id=6',
-    category: 'Herramientas'
-  }
+  
 ];
